da64104f9530dc5e90065428120115ee375966d7
Vuong Hy <vuonghy2442@hotmail.com.vn>
2018-05-17 16:41:44 +0700

    update passive mode, upload and download file

--- ftp_client/ftp_client.cpp
+++ ftp_client/ftp_client.cpp
@@ -3,6 +3,8 @@
 
 #include <codecvt>
 #include <string>
+#include <fstream>
+
 
 // convert UTF-8 string to wstring
 std::wstring utf8_to_wstring (const std::string& str)
@@ -53,15 +55,28 @@ int ftp_client::recieve_info()
 	return 0;
 }
 
-std::wstring ftp_client::getline()
+server_response ftp_client::wait_response()
 {
-	if (readable(10))
+	server_response res(L"200");
+	while (1) {
+		res = server_response(getline(true));
+		if (res.is_positive() || res.is_negative())
+			break;
+	};
+	print_response();
+	return res;
+}
+
+std::wstring ftp_client::getline(bool force)
+{
+	if (readable(10) || force)
 		recieve_info();
 
 	if (u_line) {
 		u_line--;
 		std::wstring line;
 		std::getline(response, line);
+		os << line << std::endl;
 		return line;
 	}
 	else
@@ -78,7 +93,6 @@ SOCKET ftp_client::open_port() {
 	SOCKET data_s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	bind(data_s, (sockaddr*)&add, sizeof(add));
 
-
 	getsockname(data_s, (sockaddr*)&add, &add_len);
 
 	char ipAddress[INET_ADDRSTRLEN];
@@ -86,12 +100,15 @@ SOCKET ftp_client::open_port() {
 
 	listen(data_s, 10);
 
-	send_command(L"PORT " + std::to_wstring((add.sin_addr.s_addr >> 0) & 255) + L","
+	if (!send_command(L"PORT " + std::to_wstring((add.sin_addr.s_addr >> 0) & 255) + L","
 		+ std::to_wstring((add.sin_addr.s_addr >> 8) & 255) + L","
 		+ std::to_wstring((add.sin_addr.s_addr >> 16) & 255) + L","
 		+ std::to_wstring((add.sin_addr.s_addr >> 24) & 255) + L","
 		+ std::to_wstring((add.sin_port) & 255) + L","
-		+ std::to_wstring((add.sin_port >> 8) & 255));
+		+ std::to_wstring((add.sin_port >> 8) & 255))) {
+		closesocket(data_s);
+		data_s = INVALID_SOCKET;
+	}
 
 	return data_s;
 }
@@ -104,7 +121,8 @@ ftp_client::~ftp_client() {
 	free(buffer);
 }
 
-ftp_client::ftp_client(std::wstring server, int port) {
+ftp_client::ftp_client(std::wstring server, int port, std::wostream & os) : os(os)
+{
 	int iResult = WSAStartup(0x0202, &SData);
 	if (iResult) throw WS_START_ERROR;
 
@@ -142,41 +160,175 @@ ftp_client::ftp_client(std::wstring server, int port) {
 	}
 
 	buffer = (char*)malloc(BUFFER_SIZE + 1);
-	recieve_info();
-	print_response(std::wcout);
+
+	if (wait_response().is_negative()) {
+		closesocket(s);
+		FreeAddrInfoW(host);
+		WSACleanup();
+		throw HOST_REJECT_ERROR;
+	}
 }
 
-void ftp_client::print_response(std::wostream & os)
+void ftp_client::print_response()
 {
 	while (u_line > 0)
-		os << getline() << std::endl;
+		getline(false);
+}
+
+SOCKET ftp_client::passive_mode() {
+	send_command(L"PASV", false);
+	server_response res = wait_response();
+	if (res.is_negative())
+		return INVALID_SOCKET;
+
+	size_t start = res.message.find(L'(');
+	size_t end = res.message.find(L')');
+
+	int ip1, ip2, ip3, ip4, p1, p2;
+	wchar_t c;
+
+	std::wstringstream port(res.message.substr(start + 1, end - start - 1));
+
+	port >> ip1 >> c >> ip2 >> c >> ip3 >> c >> ip4 >> c >> p1 >> c >> p2;
+
+	SOCKET data_s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	if (data_s == INVALID_SOCKET)
+		return INVALID_SOCKET;
+
+	sockaddr_in add;
+	add.sin_family = AF_INET;
+	add.sin_addr.s_addr = ip1 | (ip2 << 8) | (ip3 << 16) | (ip4 << 24);
+	add.sin_port = p1 | (p2 << 8);
+	int iResult = connect(data_s, (sockaddr*)&add, sizeof(add));
+	if (iResult)
+		return INVALID_SOCKET;
+
+	return data_s;
+}
+
+SOCKET ftp_client::send_port_command(std::wstring command) {
+	if (con_mode == ACTIVE_MODE) {
+		SOCKET data_s = open_port();
+		if (data_s == INVALID_SOCKET)
+			return data_s;
+
+		if (send_command(command)) {
+			SOCKET acc_s = accept(data_s, NULL, NULL);
+			closesocket(data_s);
+			return acc_s;
+		}
+		else
+			return INVALID_SOCKET;
+	}
+	else {
+		SOCKET data_s = passive_mode();
+		if (data_s == INVALID_SOCKET)
+			return data_s;
+
+		if (send_command(command)) {
+			return data_s;
+		}
+		else {
+			closesocket(data_s);
+			return INVALID_SOCKET;
+		}
+	}
 }
 
 void ftp_client::dir() {
-	SOCKET data_s = open_port();
-	send_command(L"LIST");
 
-	data_s = accept(data_s, NULL, NULL);
+	SOCKET data_s = send_port_command(L"LIST");
+
+	if (data_s == INVALID_SOCKET)
+		return;
 
 	int len;
 	while ((len = recv(data_s, buffer, BUFFER_SIZE, 0)) > 0) {
 		buffer[len] = 0;
 		std::wcout << utf8_to_wstring(buffer);
 	}
-
 	closesocket(data_s);
 }
 
-void ftp_client::send_command(std::wstring command) {
+void ftp_client::upload(std::wstring source, std::wstring dest) {
+	SOCKET data_s = send_port_command(L"STOR " + dest);
+
+	if (data_s == INVALID_SOCKET)
+		return;
+
+	std::ifstream f;
+	f.open(source, std::ios::in | std::ios::binary);
+
+	while (f.read(buffer, BUFFER_SIZE))
+		send(data_s, buffer, BUFFER_SIZE, 0);
+
+	if (f.gcount())
+		send(data_s, buffer, f.gcount(), 0);
+
+	f.close();
+	closesocket(data_s);
+}
+
+void ftp_client::download(std::wstring source, std::wstring dest) {
+	SOCKET data_s = send_port_command(L"RETR " + source);
+
+	if (data_s == INVALID_SOCKET)
+		return;
+
+	std::ofstream result;
+	result.open(dest, std::ios::out | std::ios::binary);
+
+	int len;
+	while ((len = recv(data_s, buffer, BUFFER_SIZE, 0)) > 0) {
+		result.write(buffer, len);
+	}
+	result.close();
+	closesocket(data_s);
+}
+
+bool ftp_client::send_command(std::wstring command, bool get_res) {
 	while (!command.empty() && (command.back() == '\n' || command.back() == '\r'))
 		command.pop_back();
 
 	if (command.empty())
-		return;
+		return true;
 
 	command += L"\r\n";
 
 	send(s, wstring_to_utf8(command).c_str(), command.length(), 0);
-	recieve_info();
-	print_response(std::wcout);
+	
+	if (!get_res)
+		return true;
+
+	return wait_response().is_positive();
+}
+
+void ftp_client::to_mode(int mode)
+{
+	if (mode != ACTIVE_MODE && mode != PASSIVE_MODE)
+		return;
+
+	this->con_mode = mode;
+}
+
+server_response::server_response(std::wstring message) : message(message)
+{
+	std::wstringstream s(message);
+	s >> code;
+}
+
+bool server_response::is(int code)
+{
+	return this->code / 100 == code;
+}
+
+bool server_response::is_positive()
+{
+	return code / 100 <= 3;
+}
+
+bool server_response::is_negative()
+{
+	return code / 100 == 4 || code / 100 == 5;
 }
--- ftp_client/ftp_client.h
+++ ftp_client/ftp_client.h
@@ -12,9 +12,32 @@
 #define CREATE_SOCKET_ERROR 3
 #define CONNECT_HOST_ERROR 4
 #define RECIEVE_SOCKET_ERROR 5
-#define DISCONECTED 6
+#define HOST_REJECT_ERROR 6
+#define DISCONECTED 7
+
+
+#define POSITIVE_PRE 1
+#define POSITIVE_COMP 2
+#define POSITIVE_INM 3
+#define NEGATIVE_TRANS 4
+#define NEGATIVE_PERM 5
+
+#define ACTIVE_MODE 1
+#define PASSIVE_MODE 2
 
 #define BUFFER_SIZE 100
+
+struct server_response
+{
+	int code;
+	std::wstring message;
+
+	server_response(std::wstring message);
+
+	bool is(int code);
+	bool is_positive();
+	bool is_negative();
+};
 
 class ftp_client
 {
@@ -22,6 +45,10 @@ private:
 	WSADATA SData;
 	SOCKET s;
 	addrinfoW *host = NULL;
+
+	int con_mode = ACTIVE_MODE;
+
+	std::wostream &os;
 
 	int u_line = 0; //number of unprocessed line
 	std::wstringstream response;
@@ -31,17 +58,25 @@ private:
 	bool readable(int wait_time);
 	int recieve_info();
 
-	std::wstring getline();
+	server_response wait_response();
+
+	std::wstring getline(bool force = false);
 	SOCKET open_port();
+	SOCKET passive_mode();
 public:
 	~ftp_client();
 
-	ftp_client(std::wstring server, int port = 21);
+	ftp_client(std::wstring server, int port = 21, std::wostream & os = std::wcout);
 
-	void print_response(std::wostream &os);
+	void print_response();
+
 
 	void dir();
-	void send_command(std::wstring command);
+	void download(std::wstring source, std::wstring dest);
+	void upload(std::wstring source, std::wstring dest);
+	bool send_command(std::wstring command, bool get_res = true);
+	SOCKET send_port_command(std::wstring command);
+	void to_mode(int mode);
 };
 
 //void recieve_thread() {
